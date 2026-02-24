5bbb528f8b0c7d3e3ef601b8beb65a32163065cb
Andromeda1101 <zhenyu-w23@mails.tsinghua.edu.cn>
2026-02-25 00:14:04 +0800

    lab1 code

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,6 @@
 //! Process management syscalls
 use crate::{
-    task::{exit_current_and_run_next, suspend_current_and_run_next},
+    task::{exit_current_and_run_next, suspend_current_and_run_next, get_current_task_syscall_count},
     timer::get_time_us,
 };
 
@@ -38,8 +38,23 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     0
 }
 
-// TODO: implement the syscall
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match trace_request {
+        0 => {
+            // Read one byte from user address `id`
+            let val = unsafe { *(id as *const u8) };
+            val as isize
+        }
+        1 => {
+            // Write lowest byte of `data` to user address `id`
+            unsafe { *(id as *mut u8) = data as u8 };
+            0
+        }
+        2 => {
+            // Query syscall count for syscall ID `id` (this call already counted)
+            get_current_task_syscall_count(id) as isize
+        }
+        _ => -1,
+    }
 }
--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -19,7 +19,7 @@ use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
 use lazy_static::*;
 use switch::__switch;
-pub use task::{TaskControlBlock, TaskStatus};
+pub use task::{TaskControlBlock, TaskStatus, MAX_SYSCALL_NUM};
 
 pub use context::TaskContext;
 
@@ -54,6 +54,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            syscall_counts: [0u32; MAX_SYSCALL_NUM],
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -168,4 +169,24 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+
+/// Increment the syscall count for the current task.
+pub fn increment_current_task_syscall_count(syscall_id: usize) {
+    if syscall_id < MAX_SYSCALL_NUM {
+        let mut inner = TASK_MANAGER.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_counts[syscall_id] += 1;
+    }
+}
+
+/// Get the syscall count for the current task.
+pub fn get_current_task_syscall_count(syscall_id: usize) -> u32 {
+    if syscall_id < MAX_SYSCALL_NUM {
+        let inner = TASK_MANAGER.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_counts[syscall_id]
+    } else {
+        0
+    }
 }
